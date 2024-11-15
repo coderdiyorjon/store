651f04529351f4f5eb7449c265caf72fc8c8ff6d
Robins Side <robinssi@student.21-school.ru>
2024-11-15 16:22:33 +0500

    ProductListView

--- products/views.py
+++ products/views.py
@@ -1,22 +1,44 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from products.models import ProductCategory, Product, Basket
 
-from django.core.paginator import Paginator
 
 # Create your views here.
 
-def index(request):
-    context = {'is_promotion': True, 'title': 'Store', }
-    return render(request, 'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
 
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page=3
-    context = {'title': 'Store - Каталог','products':(Paginator(products, per_page)).page(page_number), 'categories': ProductCategory.objects.all(), }
-    return render(request, 'products/products.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+
+# def index(request):
+#     context = {'is_promotion': True, 'title': 'Store', }
+#     return render(request, 'products/index.html', context)
+
+
+# def products(request, category_id=None, page_number=1):
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     per_page=3
+#     context = {'title': 'Store - Каталог','products':(Paginator(products, per_page)).page(page_number), 'categories': ProductCategory.objects.all(), }
+#     return render(request, 'products/products.html', context)
 
 
 @login_required
--- store/urls.py
+++ store/urls.py
@@ -13,18 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import index
+from django.contrib import admin
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls',namespace='products')),
-    path('users/', include('users.urls',namespace='users')),
-]
+from products.views import IndexView
+
+urlpatterns = [path('admin/', admin.site.urls),
+    path('', IndexView.as_view(extra_context={'title': 'Store','is_promotion':True}), name='index'),
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')), ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

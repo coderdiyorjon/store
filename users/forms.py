58600cba729dccbfef4215b05e43fa1dc69a61cb
Robins Side <robinssi@student.21-school.ru>
2024-12-04 12:36:51 +0500

    users.models.py --> EmailVerification

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,12 @@
+import uuid
+from datetime import timedelta
 # from importlib.metadata import requires
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.utils.timezone import now
 
-from users.models import User
+from users.models import User, EmailVerification
 
 
 class UserLoginForm(AuthenticationForm):
@@ -35,6 +38,12 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
+    def save(self, commit=True):
+        user=super(UserRegisterForm, self).save(commit=True)
+        expiration=now()+timedelta(days=2)
+        record = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
+        record.send_verification_email()
+        return user
 
 class UserProfileForm(UserChangeForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control py-4"}))
--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.mail import send_mail
 
 from django.contrib.auth.models import AbstractUser
 
@@ -6,3 +7,22 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    is_verified_email=models.BooleanField(default=False)
+
+class EmailVerification(models.Model):
+    code=models.UUIDField(unique=True)
+    user=models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+    expiration=models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
+
+    def send_verification_email(self):
+        send_mail(
+            'Subject',
+            'Message',
+            'noreply@example.com',  # from_email
+            [self.user.email],  # recipient_list
+            fail_silently=False,
+        )

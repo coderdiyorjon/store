edc3536a403ab934e79fdb7c399c25680998cc70
Robins Side <robinssi@student.21-school.ru>
2025-02-26 12:36:32 +0500

    Order Layout overview Transferring templates to a project

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class OrderCreateView(TemplateView):
+    template_name = 'orders/order-create.html'
